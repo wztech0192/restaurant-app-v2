80711ef796966a65a8c89cb228a27ad1f29993d7
WeiZheng <wjzheng0191@gmail.com>
2020-11-26 01:55:12 -0500

    add version tooltip

--- client/src/features/Account/AccountButton.jsx
+++ client/src/features/Account/AccountButton.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { Button, IconButton, Typography } from "@material-ui/core";
-import {
-    handleSetAccountView,
-    getAccountToken,
-    ACCOUNT_VIEW,
-    getAccountRole
-} from "./accountSlice";
+import { Button, IconButton, Tooltip, Typography } from "@material-ui/core";
+import { handleSetAccountView, getAccountToken, ACCOUNT_VIEW, getAccountRole } from "./accountSlice";
 import { useDispatch, useSelector } from "react-redux";
 import AccountIcon from "@material-ui/icons/AccountCircle";
 import { isManager } from "./roleChecker";
-
+import packageJson from "../../../package.json";
 const AccountButton = () => {
     const dispatch = useDispatch();
     const isLogin = useSelector(getAccountToken);
@@ -19,12 +14,11 @@ const AccountButton = () => {
     return isLogin ? (
         <>
             {manager && <Typography>Manager</Typography>}
-            <IconButton
-                color="inherit"
-                onClick={dispatch(handleSetAccountView(ACCOUNT_VIEW.PROFILE))}
-            >
-                <AccountIcon />
-            </IconButton>
+            <Tooltip title={`App Version ${packageJson.version}`}>
+                <IconButton color="inherit" onClick={dispatch(handleSetAccountView(ACCOUNT_VIEW.PROFILE))}>
+                    <AccountIcon />
+                </IconButton>
+            </Tooltip>
         </>
     ) : (
         <Button color="inherit" onClick={dispatch(handleSetAccountView(ACCOUNT_VIEW.LOGIN))}>
